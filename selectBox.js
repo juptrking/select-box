599f11c563f2550e55b6d23414925c9cd8b62fcf
Will Thomas <will@mybuilder.com>
2016-04-05 11:22:33 +0100

    Update selectBox.js

--- selectBox.js
+++ selectBox.js
@@ -1,5 +1,7 @@
 var SelectBox = (function(){
     function wrapSelectBox(elem){
+        // make sure the target is viable and isn't already wrapped
+        // wrap the select box with the dacade mark up
         if (!elem.classList.contains('unstyled') && !elem.parentNode.classList.contains('selector')) {
             var myValue = elem.options[elem.selectedIndex].text;
             var modified = '<div class="selector"><span style="-webkit-user-select: none;">' + myValue + '</span>' + elem.outerHTML + '</div>';
@@ -7,6 +9,8 @@ var SelectBox = (function(){
         }
     }
     function updateSelectBox(elem){
+        // update text in facade when select box value is changed
+        // make sure the select box is already wrapped
         var wrap = elem.parentNode;
         if (wrap.classList.contains('selector')) {
             var myValue = elem.options[elem.selectedIndex].text;
@@ -15,10 +19,13 @@ var SelectBox = (function(){
         }
     }
     function toggleSelectBox(elem){
+        // simple class toggle for focus and blur states
         var wrap = elem.parentNode;
         if (wrap.classList.contains('selector')) wrap.classList.toggle('focus');
     }
     function selectEvent(myEvent, callback){
+        // event handler
+        // check to assure target is a select box
         document.addEventListener(myEvent, function(e){
             var elem = e.target;
             if (elem.type && elem.type === 'select-one') {
@@ -28,16 +35,22 @@ var SelectBox = (function(){
         }, true);
     }
     function initSelectBox(elems) {
+        // check to see if a target has been specified, if not grab all select boxes available
+        // if the selection isn't already in an array, put it in one
+        // run a wrap function on each item in the array
         if (undefined === elems) elems = Array.prototype.slice.call(document.getElementsByTagName("select"));
         var matches = (Array.isArray(elems)) ? elems : [elems];
         matches.forEach(wrapSelectBox);
     }
     document.addEventListener("DOMContentLoaded", function() {
+        // initialise the script on document ready
+        // capture all relevant events
         initSelectBox();
         selectEvent('change',updateSelectBox);
         selectEvent('focus',toggleSelectBox);
         selectEvent('blur',toggleSelectBox);
     });
+    // return a method to allow select boxes to be wrapped manually after document ready
     return {
         wrap: initSelectBox
     };
